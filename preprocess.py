5587b327e55bd04914da5d04efec7a5bbbae68d1
aaronjutzi <ajutzi@flemingcollege.ca>
2021-06-08 17:19:11 -0400

    added qgis code

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,10 @@ import subprocess
 
 import csv
 
+import pandas as pd
+
+import ast
+
 import time
 
 # import the PostgreSQL adapter for Python
@@ -64,7 +68,7 @@ txt_file = str('"' + local_file.replace("n.Z", "txt") + '"')
 
 # setting up a command line code to unzip files using 7zip
 # the output file path will need to be congifured
-one = str('7z e ' + local_file + ' -oC:\CollaborativeProject\output')
+one = str('7z e ' + local_file + ' -oC:\CollaborativeProject\output\\results')
 
 # setting up a command line code to rename the unzipped file into a txt file
 two = str('rename ' + unzipped + " " + txt_file)
@@ -153,6 +157,151 @@ sqlAlter = "UPDATE "+name_Table+" SET GEOM = ST_SetSRID(ST_MakePoint(ws.lon, ws.
 cursor.execute(sqlAlter)
 
 postgresConnection.commit()
+
+
+weather_file = txt_file.replace(".txt", ".csv")
+
+txt_file2 = txt_file.replace('"',"")
+weather_file = weather_file.replace('"',"")
+
+
+header = ['STN', 'DATE','HR', 'TEMP', 'WIND', 'Y', 'X']
+
+df = pd.read_csv('latlon.csv')
+
+first_column = df[df.columns[0]]
+second_column = df[df.columns[1]]
+print(first_column)
+print(second_column)
+
+count = 0
+
+with open(txt_file2, "r") as file, open(weather_file, "w", encoding='UTF8', newline='') as new_file:  
+    
+    writer = csv.writer(new_file)
+    writer.writerow(header)
+    
+    for line in file:
+
+        try:
+            if line[5:9] in list4:
+                
+                for x in range(26):
+                    
+                    next(file)
+                
+                current = file.readline()
+                
+                writer.writerow([line[5:9], current[0:8], current[9:13], ast.literal_eval(current[65:70].replace(" ", "")), 
+                ast.literal_eval(current[81:84].replace(" ", "")), first_column[count], second_column[count]])
+
+                # writer.writerow([line[5:9], current[0:8], current[9:13], 
+                # first_column[count], second_column[count]])
+                
+                count += 1
+
+        except IndexError:
+            for x in range(30):
+                next(file)
+
+
+
+import sys
+from qgis.core import *
+
+
+# Supply path to qgis install location
+QgsApplication.setPrefixPath("'C:/OSGEO4~1/apps/qgis'", True)
+
+# Create a reference to the QgsApplication.  Setting the
+# second argument to False disables the GUI.
+qgs = QgsApplication([], False)
+
+# Load providers
+qgs.initQgis()
+
+sys.path.append(r'C:\OSGeo4W64\apps\qgis\python\plugins')
+
+import processing
+from processing.core.Processing import Processing
+Processing.initialize()
+
+
+
+l = weather_file
+
+output = l.replace(".csv", ".gpkg")
+print(output)
+
+# Create points layer from table
+alg_params = {
+    'INPUT': l,
+    'MFIELD': '',
+    'OUTPUT': output,
+    'TARGET_CRS': QgsCoordinateReferenceSystem('EPSG:4326'),
+    'XFIELD': 'X',
+    'YFIELD': 'Y',
+    'ZFIELD': '',
+    'OUTPUT': output
+}
+
+processing.run('native:createpointslayerfromtable', alg_params)
+
+
+layer=QgsVectorLayer(output)
+
+from PyQt5.QtCore import QVariant
+
+layer_provider=layer.dataProvider()
+layer_provider.addAttributes([QgsField("T",QVariant.Double)])
+layer_provider.addAttributes([QgsField("W",QVariant.Double)])
+layer.updateFields()
+print(layer.fields().names())
+
+
+expression1 = QgsExpression('"TEMP"')
+expression2 = QgsExpression('"WIND"')
+
+context = QgsExpressionContext()
+context.appendScopes(\
+QgsExpressionContextUtils.globalProjectLayerScopes(layer))
+
+
+
+with edit(layer):
+    for f in layer.getFeatures():
+        context.setFeature(f)
+        f['T'] = expression1.evaluate(context)
+        f['W'] = expression2.evaluate(context)
+        layer.updateFeature(f)
+    
+raster = output.replace(".gpkg", ".tif")    
+
+param = { 'DISTANCE_COEFFICIENT' : 4,
+ 'EXTENT' : '-95.154825942,-74.343495817,41.681435425,56.859036233 [EPSG:4326]', 
+ 'INTERPOLATION_DATA' : str(output + '|layername=SCRIBE::~::0::~::8::~::0'),
+ 'OUTPUT' : raster, 'PIXEL_SIZE' : 0.1 }
+
+
+processing.run("qgis:idwinterpolation", param)
+
+
+# { 'ALPHA_BAND' : False, 'CROP_TO_CUTLINE' : True, 'DATA_TYPE' : 0, 
+# 'EXTRA' : '', 
+# 'INPUT' : 'C:/CollaborativeProject/output/SCRIBE.NWCSTG.06.06.17Z.tif', 
+# 'KEEP_RESOLUTION' : False, 'MASK' : 'C:/CollaborativeProject/ontario.gpkg|layername=ontario', 
+# 'MULTITHREADING' : False, 
+# 'NODATA' : None, 
+# 'OPTIONS' : '', 
+# 'OUTPUT' : 'C:/CollaborativeProject/output/clip_test.tif', 
+# 'SET_RESOLUTION' : False, 'SOURCE_CRS' : QgsCoordinateReferenceSystem('EPSG:4326'), 
+# 'TARGET_CRS' : QgsCoordinateReferenceSystem('EPSG:4326'), 
+# 'X_RESOLUTION' : None, 'Y_RESOLUTION' : None }
+
+
+
+qgs.exitQgis()
+
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
